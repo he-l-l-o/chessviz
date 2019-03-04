b91c91667f6df22841cea500cc59ca9f1fc5c13b
Artem <he-l-l-o@mail.ru>
2019-03-04 11:23:18 +0700

    add coordinates

--- main.cpp
+++ main.cpp
@@ -4,8 +4,13 @@ using namespace std;
 
 void out(vector<vector<char>>& arr)
 {
+    cout << "  ";
+    for (unsigned j = 0; j < arr[0].size(); j++)
+        cout << (char)('a' + j) << " ";
+    cout << endl;
     for (unsigned i = 0; i < arr.size(); i++) {
         // cout << "----------------\n";
+        cout << i + 1;
         for (unsigned j = 0; j < arr[0].size(); j++) {
             cout << "|";
             if (arr[i][j] != ' ') {
