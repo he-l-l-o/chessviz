95088aedbf1837c773e9d69d9b6f3e76bfb95ad6
Artem <he-l-l-o@mail.ru>
2019-04-01 11:01:09 +0700

    move board initialization to a separate file

---
+++ src/board_start.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <vector>
+using namespace std;
+void init(vector<vector<char>>& arr);
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "board_start.h"
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -24,27 +25,6 @@ void out(vector<vector<char>>& arr)
         cout << "|\n";
     }
     // cout << "----------------\n";
-}
-void init(vector<vector<char>>& arr)
-{
-    arr[0][0] = 'r';
-    arr[0][1] = 'n';
-    arr[0][2] = 'b';
-    arr[0][3] = 'q';
-    arr[0][4] = 'k';
-    arr[0][5] = 'b';
-    arr[0][6] = 'n';
-    arr[0][7] = 'r';
-    arr[1] = vector<char>(8, 'p');
-    arr[7][0] = 'R';
-    arr[7][1] = 'N';
-    arr[7][2] = 'B';
-    arr[7][3] = 'Q';
-    arr[7][4] = 'K';
-    arr[7][5] = 'B';
-    arr[7][6] = 'N';
-    arr[7][7] = 'R';
-    arr[6] = vector<char>(8, 'P');
 }
 
 int main()
