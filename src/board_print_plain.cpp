38787fa84f4888cc9408a75a244a8ca3c3ab8363
Artem <he-l-l-o@mail.ru>
2019-04-14 22:43:17 +0700

    move board print to a separate file

---
+++ src/board_print_plain.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void out(vector<vector<char>>& arr)
+{
+    cout << "  ";
+    for (unsigned j = 0; j < arr[0].size(); j++)
+        cout << (char)('a' + j) << " ";
+    cout << endl;
+    for (unsigned i = 0; i < arr.size(); i++) {
+        // cout << "----------------\n";
+        cout << i + 1;
+        for (unsigned j = 0; j < arr[0].size(); j++) {
+            cout << "|";
+            if (arr[i][j] != ' ') {
+                cout << arr[i][j];
+            } else if ((i + j) % 2 == 0) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << "|\n";
+    }
+    // cout << "----------------\n";
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,8 @@
+#include "board_print_plain.h"
 #include "board_start.h"
 #include <iostream>
 #include <vector>
 using namespace std;
-
-void out(vector<vector<char>>& arr)
-{
-    cout << "  ";
-    for (unsigned j = 0; j < arr[0].size(); j++)
-        cout << (char)('a' + j) << " ";
-    cout << endl;
-    for (unsigned i = 0; i < arr.size(); i++) {
-        // cout << "----------------\n";
-        cout << i + 1;
-        for (unsigned j = 0; j < arr[0].size(); j++) {
-            cout << "|";
-            if (arr[i][j] != ' ') {
-                cout << arr[i][j];
-            } else if ((i + j) % 2 == 0) {
-                cout << "*";
-            } else {
-                cout << " ";
-            }
-        }
-        cout << "|\n";
-    }
-    // cout << "----------------\n";
-}
 
 int main()
 {
