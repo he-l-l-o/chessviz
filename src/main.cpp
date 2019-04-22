f14ff19917c36a0cfff1df3c1f38d03c994a01e5
Artem <he-l-l-o@mail.ru>
2019-04-22 10:07:22 +0700

    change tab size

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "board_print_plain.h"
+#include "board_read.h"
 #include "board_start.h"
 #include <iostream>
 #include <vector>
@@ -6,7 +7,7 @@ using namespace std;
 
 int main()
 {
-    vector<vector<char>> arr(8, vector<char>(8, ' '));
-    init(arr);
-    out(arr);
+  vector<vector<char>> arr(8, vector<char>(8, ' '));
+  init(arr);
+  out(arr);
 }
